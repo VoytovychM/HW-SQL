26f2ea8975ebbdae2bf5517accad50eb955bbfc4
VoytovychM <151627487+VoytovychM@users.noreply.github.com>
2024-03-06 20:32:20 +0100

    Update 3.sql
    Homework No.3

--- 3.sql
+++ 3.sql
@@ -1,1 +1,23 @@
+--1. Вывести данные о товарах не из категорий 2 и 4 и не от поставщиков 2 и 3 с ценой от 20 до 90 EUR (проекция: название, ценаснаценкой_6%)--
+SELECT ProductName, Price * 1.06 AS Price_up FROM Products
+WHERE 
+NOT CategoryID IN (2, 4)
+AND
+NOT SupplierID IN (2, 3)
+AND 
+Price BETWEEN 20 AND 90
 
+--2. Вывести клиентов не из Germany, имена которых не заканчиваются на букву t --
+SELECT * FROM Customers
+WHERE
+NOT Country = 'Germany'
+AND
+NOT CustomerName LIKE '%t'
+
+--3. Вывести два самых дешевых товара с ценой от 10 EUR и более (проекция: название, ценасоскидкой_20%)--
+SELECT ProductName, Price * .8 AS Price_down
+FROM Products
+WHERE
+Price >= 10 
+ORDER BY Price ASC
+LIMIT 2
