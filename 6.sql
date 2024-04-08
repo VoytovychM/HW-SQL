b28caed65580f4915082d61a1519a148a41b7992
VoytovychM <151627487+VoytovychM@users.noreply.github.com>
2024-04-08 18:27:19 +0200

    Create 6.sql
    Initial commit  homework6

---
+++ 6.sql
@@ -0,0 +1,34 @@
+--1. Вывести стоимость заказа 10258 --
+SELECT SUM(OrderDetails.Quantity * Products.Price) AS TotalOderCost
+FROM OrderDetails
+JOIN Products ON OrderDetails.ProductID = Products.ProductID
+
+WHERE
+OrderDetails.OrderID = 10258
+
+--2. Вывести кол/распределение заказов по клиентам (проекция: имяклиента, ко-во заказов)--
+SELECT Customers.CustomerName, COUNT(*) AS total_orders
+FROM Orders
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+GROUP BY Customers.CustomerID
+
+--3. Вывести кол/распределение заказов по менеджерам (проекция: фамилияменеджера, ко-возаказов) --
+SELECT Employees.LastName, COUNT(*) AS total_orders
+FROM Orders
+JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+GROUP BY Employees.EmployeeID
+
+--4. Вывести минимальную стоимость товаров для каждой категории (проекция: названиекатегории, минстоимость_товаров) --
+SELECT Categories.CategoryName, MIN(Price) AS min_price
+FROM Products
+JOIN Categories ON Products.CategoryID = Categories.CategoryID 
+GROUP BY Categories.CategoryID
+
+--5. Вывести данные о заказах (проекция: номерзаказа, стоимостьзаказа)--
+SELECT OrderDetails.OrderID, SUM(OrderDetails.Quantity * Products.Price) AS TotalOderCost
+FROM OrderDetails
+JOIN Products ON OrderDetails.ProductID = Products.ProductID
+GROUP BY OrderDetails.OrderID
+
+--6. Вывести доход каждого менеджера за весь период, исходя из ставки в 5% от суммы его заказов (проекция: фамилия_менеджера, доход)--
+
