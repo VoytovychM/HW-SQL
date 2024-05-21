983b65674a5fae3f2719ca69363df754e9971208
VoytovychM <151627487+VoytovychM@users.noreply.github.com>
2024-05-21 15:36:34 +0200

    Update 8.sql
    update

--- 8.sql
+++ 8.sql
@@ -1,7 +1,22 @@
---Создать таблицу друзей с использованием механизма валидации данных в SQL--
+--1.Создать таблицу друзей с использованием механизма валидации данных в SQL--
+
+create table friends (
+    id int primary key auto_increment,
+    user_id int,
+    friend_id int,
+    created_at timestamp not null default current_timestamp, 
+    foreign key (user_id) references users(user_id),
+    foreign key (friend_id) references users(user_id),
+    unique key unique_friendship (user_id, friend_id),
+    is_deactivated bool default false);
+
+--2. Добавить в таблицу друзей несколько записей--
+insert into friends (user_id, friend_id) values (1, 2), (3, 4), (4, 1), (3, 2);
+
 --3.Вывести сумму всех транзакций отдельно по каждой валюте (в упорядоченном виде)--
-SELECT accounts.currency, SUM(amount) AS total_amount
-FROM transactions
-JOIN accounts ON accounts.id = transactions.id
-GROUP BY currency
-ORDER BY currency;
+
+select accounts.currency, SUM(amount) as total_amount
+from transactions
+join accounts on accounts.id = transactions.id
+group by currency
+order by currency;
