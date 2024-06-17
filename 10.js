c4d5de2a972824d7d753062925a3776933871ce0
VoytovychM <151627487+VoytovychM@users.noreply.github.com>
2024-06-17 19:42:34 +0200

    Update 10.js
    homework20240613

--- 10.js
+++ 10.js
@@ -1,1 +1,35 @@
+--1. Вывести названия двух произвольных треков с тегом mega--
+db.videos.aggregate([
+    {
+        $sample: { size: 2 }},
+     {$project: {tags: "mega"}}   
+])
+​
+--2.Вывести имя юзера с самым минимальным балансом--
+db.users.aggregate([
+    
+    { $sort: { balance: 1 } },
+    { $limit: 1 },
+     { $project: { _id: 0, fullname: 1 } }
+])
+
+--3. Используя метод countDocuments(), вывести ко-во заблокированных юзеров с балансом от 10 до 1000 EUR (вкл.)--
+db.users.countDocuments({ 
+    
+    is_blocked: true,
+    balance: {$gte: 10, $lte: 1000 }
+})
+
+--4. Используя метод aggregate(), вывести ко-во незаблокированных юзеров не из Germany и не из France с балансом до 1000 EUR (не вкл.)--
+db.users.aggregate([
+    
+    {$match: {is_blocked: {$ne: true},
+    country: {$nin: ['Germany', 'France']},
+    balance: {$lt:1000}}
+    },
+    {$count:"total_number_of_unblocked_users_out_of_Germany_and_France"}
+])
+
+--5. Также очень коротко опишите, как работает агрегация в MongoDB--
+--Агрегация обрабатывает данные поэтапно через операторов: фильтр, сортировку и тд--
 
